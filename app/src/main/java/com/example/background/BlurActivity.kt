fa55c3bee184df1a9b002be60e4f04262655c482
jcabal <jcabalf3@gmail.com>
2022-03-15 18:22:39 +0100

    Trabajo en segundo plano con WorkerManager (Funciona)

--- app/src/main/java/com/example/background/BlurActivity.kt
+++ app/src/main/java/com/example/background/BlurActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.background
 
 import android.content.Intent
@@ -21,9 +5,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.background.databinding.ActivityBlurBinding
 import androidx.lifecycle.Observer
 import androidx.work.WorkInfo
+import com.example.background.databinding.ActivityBlurBinding
 
 class BlurActivity : AppCompatActivity() {
 
@@ -35,6 +19,12 @@ class BlurActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBlurBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBlurBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
+
         // Setup view output image file button
         binding.seeFileButton.setOnClickListener {
             viewModel.outputUri?.let { currentUri ->
@@ -44,9 +34,11 @@ class BlurActivity : AppCompatActivity() {
                 }
             }
         }
-        // In onCreate()
+
         // Hookup the Cancel button
         binding.cancelButton.setOnClickListener { viewModel.cancelWork() }
+
+        viewModel.outputWorkInfos.observe(this, workInfosObserver())
     }
 
     private fun workInfosObserver(): Observer<List<WorkInfo>> {
